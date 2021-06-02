8a471b63c58ebbdaf4e54eb45f17af07f81011e3
AhmadNAwasrah <eng.ahmad.nawasreh@gmail.com>
2021-06-02 03:10:26 +0300

    Food Lovers

--- js/app.js
+++ js/app.js
@@ -3,43 +3,52 @@ let formEl = document.getElementById('formInfo');
 let tableEl = document.getElementById('order');
 
 let objArr = [];
-function Order(name, item) {
+function Order(name, foodType,imgPath) {
     this.name = name;
-    this.item = item;
+    this.foodType = `Food Type: ${foodType}`;
+    this.price = price;
+    this.imgPath = `img/${foodType}.jpg`;
     objArr.push(this);
 
 }
+function randomPrice() {
+    price = Math.floor((Math.random() * 20) + 10);
+}
+console.log(randomPrice());
 formEl.addEventListener('submit', handler);
 function handler(event) {
-    event.preventDefault();
+    event.preventDefault(); 
     let cusName = event.target.custmerName.value;
     let foodItem = event.target.foodList.value;
     let newobj = new Order(cusName, foodItem);
-    setLocalStorage();
-    orderobject.render();
+   
+    //orderobject.render();
     newobj.render();
+     setLocalStorage();
 }
 Order.prototype.render = function () {
-    tableEl.textContent='';
+    tableEl.textContent = '';
+    let thEl=document.createElement('th');
+    tableEl.appendChild(thEl);
+    thEl.textContent='Order Image';
+    let thEl1=document.createElement('th');
+    tableEl.appendChild(thEl1);
+    thEl1.textContent='Order Detiles';
+
     for (let i = 0; i < objArr.length; i++) {
         let trEl = document.createElement('tr');
         tableEl.appendChild(trEl);
         let tdEl = document.createElement('td');
         trEl.appendChild(tdEl);
-        let imgEl=document.createElement('img');
+        let imgEl = document.createElement('img');
         tdEl.appendChild(imgEl);
-        if(newobj.foodItem=='Shawrma')
-        imgEl.src='/img/shawrma.png'
-        else if (newobj.foodItem=='Burger')
-        imgEl.src='/img/burger.jpg'
-        else if (newobj.foodItem=='Pizza')
-        imgEl.src='/img/pitza.jpg';
+        imgEl.setAttribute('src', objArr[i].imgPath);
 
-        tdEl.textContent=imgEl;
-let tdEl1=document.createElement('td');
-trEl.appendChild(tdEl1);
-tdEl1='Custmer Name=' + objArr[i].name+'/n Food Type ='+ objArr[i].foodItem+Math.floor((Math.random() * 10) + 1);
-
+        //  tdEl.textContent = imgEl;
+        let tdEl1 = document.createElement('td');
+        trEl.appendChild(tdEl1);
+        tdEl1.textContent = 'Custmer Name :' + objArr[i].name + objArr[i].foodType + 'Price: '+objArr[i].price;
+       
     }
 
     console.log(objArr);
@@ -47,23 +56,24 @@ tdEl1='Custmer Name=' + objArr[i].name+'/n Food Type ='+ objArr[i].foodItem+Math
 
 }
 
-function setLocalStorage()
-{let data=JSON.stringify(objArr);
-localStorage.setItem(name,data);
+function setLocalStorage() {
+    let data = JSON.stringify(objArr);
+    localStorage.setItem(name, data);
+  
 }
 
-function getFromLocalStorage()
-{let strObj=localStorage.getItem(name);
-let normalobj=JSON.parse(strObj);
-if(normalobj !=null)
-{let orderobject;
-    for(let i=0;i<normalobj.length;i++)
-    {orderobject=new Order(normalobj[i].name,normalobj[i].foodItem)
-    
+function getFromLocalStorage() {
+    let strObj = localStorage.getItem(name);
+    let normalobj = JSON.parse(strObj);
+    if (normalobj != null) {
+        let orderobject;
+        for (let i = 0; i < normalobj.length; i++) {
+            orderobject = new Order(normalobj[i].name, normalobj[i].foodItem)
+
+        }
+
+        orderobject.render();
+
     }
-
-orderobject.render();
-
-}
 
 }
